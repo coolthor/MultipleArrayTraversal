98ea5d8cb8e999b68fabf222444ea5ed3285367b
Thor Lin <coolthor@gmail.com>
2020-11-05 23:48:44 +0800

    establish new function

--- WindowsFormsApp1/Main.Designer.cs
+++ WindowsFormsApp1/Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// 設計工具所需的變數。
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(787, 436);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
---
+++ WindowsFormsApp1/Main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            List<int> a = new List<int>{ 1, 2, 3, 4 };
+            List<int> b = new List<int> { 5, 6, 7, 8};
+            List<int> c = new List<int> { 9, 10, 11, 12};
+            List<int> d = new List<int> { 13, 14, 15, 16 };
+
+            List<List<int>> summary = new List<List<int>> {a,b,c,d};
+            try
+            {
+                int iCnt = 0;
+                for (int i = 0; i < summary.Count; i++)
+                {
+                    summary[i].ForEach(o =>
+                    {
+                        if (i + 1 == summary.Count)
+                            return;
+
+                        for (int j = i + 1; j < summary.Count; j++)
+                        {
+                            summary[j].ForEach(t =>
+                            {
+                                iCnt++;
+                                listBox1.Items.Add(o.ToString() + "*" + t.ToString());
+                            });
+                        }
+                    });
+                }
+                listBox1.Items.Add("count:"+iCnt.ToString());
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
